9624cb98d3a63264110cc4fe735802e9f7d73589
Stephen "Christian" Kocsis <89398186+stephenkocsis14@users.noreply.github.com>
2023-02-21 15:16:29 -0500

    Update 2D_scatterplots_ggplot2.R

--- 2D_scatterplots_ggplot2.R
+++ 2D_scatterplots_ggplot2.R
@@ -1,1 +1,228 @@
+# Basic scatter plots
 
+library(ggplot2)
+# Basic scatter plot
+ggplot(mtcars, aes(x=wt, y=mpg)) + geom_point()
+# Change the point size, and shape
+ggplot(mtcars, aes(x=wt, y=mpg)) +
+  geom_point(size=2, shape=23)
+# Change the point size
+ggplot(mtcars, aes(x=wt, y=mpg)) + 
+  geom_point(aes(size=qsec))
+
+# Label points in the scatter plot
+ggplot(mtcars, aes(x=wt, y=mpg)) +
+  geom_point() + 
+  geom_text(label=rownames(mtcars))
+
+# Add regression lines
+# The functions below can be used to add regression lines to a scatter plot: geom_smooth() and stat_smooth() & geom_abline()
+
+# Simplified format
+geom_smooth(method="auto", se=TRUE, fullrange=FALSE, level=0.95)
+# Add the regression line
+ggplot(mtcars, aes(x=wt, y=mpg)) + 
+  geom_point()+
+  geom_smooth(method=lm)
+# Remove the confidence interval
+ggplot(mtcars, aes(x=wt, y=mpg)) + 
+  geom_point()+
+  geom_smooth(method=lm, se=FALSE)
+# Loess method
+ggplot(mtcars, aes(x=wt, y=mpg)) + 
+  geom_point()+
+  geom_smooth()
+
+# Change the appearance of points and lines
+
+# Change the point colors and shapes
+# Change the line type and color
+ggplot(mtcars, aes(x=wt, y=mpg)) + 
+  geom_point(shape=18, color="blue")+
+  geom_smooth(method=lm, se=FALSE, linetype="dashed",
+             color="darkred")
+# Change the confidence interval fill color
+ggplot(mtcars, aes(x=wt, y=mpg)) + 
+  geom_point(shape=18, color="blue")+
+  geom_smooth(method=lm,  linetype="dashed",
+             color="darkred", fill="blue")
+
+# Change the point color/size/shape automatically
+
+# Change point shapes by the levels of cyl
+ggplot(mtcars, aes(x=wt, y=mpg, shape=cyl)) +
+  geom_point()
+# Change point shapes and colors
+ggplot(mtcars, aes(x=wt, y=mpg, shape=cyl, color=cyl)) +
+  geom_point()
+# Change point shapes, colors and sizes
+ggplot(mtcars, aes(x=wt, y=mpg, shape=cyl, color=cyl, size=cyl)) +
+  geom_point()
+
+# Add regression lines for each group
+# Add regression lines
+ggplot(mtcars, aes(x=wt, y=mpg, color=cyl, shape=cyl)) +
+  geom_point() + 
+  geom_smooth(method=lm)
+# Remove confidence intervals
+# Extend the regression lines
+ggplot(mtcars, aes(x=wt, y=mpg, color=cyl, shape=cyl)) +
+  geom_point() + 
+  geom_smooth(method=lm, se=FALSE, fullrange=TRUE)
+# Fill regression lines with variance
+ggplot(mtcars, aes(x=wt, y=mpg, color=cyl, shape=cyl)) +
+  geom_point() + 
+  geom_smooth(method=lm, aes(fill=cyl))
+
+# Change the point color/size/shape manually
+
+# Change point shapes and colors manually
+ggplot(mtcars, aes(x=wt, y=mpg, color=cyl, shape=cyl)) +
+  geom_point() + 
+  geom_smooth(method=lm, se=FALSE, fullrange=TRUE)+
+  scale_shape_manual(values=c(3, 16, 17))+ 
+  scale_color_manual(values=c('#999999','#E69F00', '#56B4E9'))+
+  theme(legend.position="top")
+  
+# Change the point sizes manually
+ggplot(mtcars, aes(x=wt, y=mpg, color=cyl, shape=cyl))+
+  geom_point(aes(size=cyl)) + 
+  geom_smooth(method=lm, se=FALSE, fullrange=TRUE)+
+  scale_shape_manual(values=c(3, 16, 17))+ 
+  scale_color_manual(values=c('#999999','#E69F00', '#56B4E9'))+
+  scale_size_manual(values=c(2,3,4))+
+  theme(legend.position="top")
+# scale_color_brewer() : to use color palettes from RColorBrewer package & scale_color_grey() : to use grey color palettes
+p <- ggplot(mtcars, aes(x=wt, y=mpg, color=cyl, shape=cyl)) +
+  geom_point() + 
+  geom_smooth(method=lm, se=FALSE, fullrange=TRUE)+
+  theme_classic()
+# Use brewer color palettes
+p+scale_color_brewer(palette="Dark2")
+# Use grey scale
+p + scale_color_grey()
+
+# Add marginal rugs to a scatterplot
+# using the function geom_rug(sides ="bl")
+
+# Add marginal rugs
+ggplot(mtcars, aes(x=wt, y=mpg)) +
+  geom_point() + geom_rug()
+# Change colors
+ggplot(mtcars, aes(x=wt, y=mpg, color=cyl)) +
+  geom_point() + geom_rug()
+# Add marginal rugs using faithful data
+ggplot(faithful, aes(x=eruptions, y=waiting)) +
+  geom_point() + geom_rug()
+
+# Scatter plots with the 2D density estimation
+
+# Scatter plot with the 2d density estimation
+sp <- ggplot(faithful, aes(x=eruptions, y=waiting)) +
+  geom_point()
+sp + geom_density_2d()
+# Gradient color
+sp + stat_density_2d(aes(fill = ..level..), geom="polygon")
+# Change the gradient color
+sp + stat_density_2d(aes(fill = ..level..), geom="polygon")+
+  scale_fill_gradient(low="blue", high="red")
+
+# Scatter plots with elipses
+
+# One ellipse arround all points
+ggplot(faithful, aes(waiting, eruptions))+
+  geom_point()+
+  stat_ellipse()
+# Ellipse by groups
+p <- ggplot(faithful, aes(waiting, eruptions, color = eruptions > 3))+
+  geom_point()
+p + stat_ellipse()
+# Change the type of ellipses: possible values are "t", "norm", "euclid"
+p + stat_ellipse(type = "norm")
+
+# Scatter plot with rectangular bins
+# geom_bin2d() for adding a heatmap of 2d bin counts
+# stat_bin_2d() for counting the number of observation in rectangular bins
+# stat_summary_2d() to apply function for 2D rectangular bins
+
+# Plot
+p <- ggplot(diamonds, aes(carat, price))
+p + geom_bin2d()
+# Change the number of bins
+p + geom_bin2d(bins=10)
+# Or specify the width of bins
+p + geom_bin2d(binwidth=c(1, 1000))
+
+# Scatter plot with marginal density distribution plot
+
+# Step 1/3: create some data
+set.seed(1234)
+x <- c(rnorm(500, mean = -1), rnorm(500, mean = 1.5))
+y <- c(rnorm(500, mean = 1), rnorm(500, mean = 1.7))
+group <- as.factor(rep(c(1,2), each=500))
+df <- data.frame(x, y, group)
+head(df)
+# Step 2/3: Create the plots
+# scatter plot of x and y variables
+# color by groups
+scatterPlot <- ggplot(df,aes(x, y, color=group)) + 
+  geom_point() + 
+  scale_color_manual(values = c('#999999','#E69F00')) + 
+  theme(legend.position=c(0,1), legend.justification=c(0,1))
+scatterPlot
+# Marginal density plot of x (top panel)
+xdensity <- ggplot(df, aes(x, fill=group)) + 
+  geom_density(alpha=.5) + 
+  scale_fill_manual(values = c('#999999','#E69F00')) + 
+  theme(legend.position = "none")
+xdensity
+# Marginal density plot of y (right panel)
+ydensity <- ggplot(df, aes(y, fill=group)) + 
+  geom_density(alpha=.5) + 
+  scale_fill_manual(values = c('#999999','#E69F00')) + 
+  theme(legend.position = "none")
+ydensity
+# Create a blank placeholder plot
+blankPlot <- ggplot()+geom_blank(aes(1,1))+
+  theme(plot.background = element_blank(), 
+   panel.grid.major = element_blank(),
+   panel.grid.minor = element_blank(), 
+   panel.border = element_blank(),
+   panel.background = element_blank(),
+   axis.title.x = element_blank(),
+   axis.title.y = element_blank(),
+   axis.text.x = element_blank(), 
+   axis.text.y = element_blank(),
+   axis.ticks = element_blank()
+     )
+# Step 3/3: Put the plots together
+install.packages("gridExtra")
+library("gridExtra")
+grid.arrange(xdensity, blankPlot, scatterPlot, ydensity, 
+        ncol=2, nrow=2, widths=c(4, 1.4), heights=c(1.4, 4))
+
+# Customized scatter plots
+
+# Basic scatter plot
+ggplot(mtcars, aes(x=wt, y=mpg)) + 
+  geom_point()+
+  geom_smooth(method=lm, color="black")+
+  labs(title="Miles per gallon \n according to the weight",
+       x="Weight (lb/1000)", y = "Miles/(US) gallon")+
+  theme_classic()  
+# Change color/shape by groups
+# Remove confidence bands
+p <- ggplot(mtcars, aes(x=wt, y=mpg, color=cyl, shape=cyl)) + 
+  geom_point()+
+  geom_smooth(method=lm, se=FALSE, fullrange=TRUE)+
+  labs(title="Miles per gallon \n according to the weight",
+       x="Weight (lb/1000)", y = "Miles/(US) gallon")
+p + theme_classic()  
+
+# Change colors manually
+# Continuous colors
+p + scale_color_brewer(palette="Paired") + theme_classic()
+# Discrete colors
+p + scale_color_brewer(palette="Dark2") + theme_minimal()
+# Gradient colors
+p + scale_color_brewer(palette="Accent") + theme_minimal()
